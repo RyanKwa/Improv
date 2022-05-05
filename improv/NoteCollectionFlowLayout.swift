d12b7206b52c1e6c9e654d989a5eaf92d90b6114
Ryan Vieri Kwa <ryan.kwa24@gmail.com>
2022-05-05 20:00:42 +0700

    Add request permission for microphone and some comment

--- improv/NoteCollectionFlowLayout.swift
+++ improv/NoteCollectionFlowLayout.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class NoteCollectionFlowLayout: UICollectionViewFlowLayout {
+    //cell size
     var contentWidth = 390
     var contentHeight = 549
     override func prepare() {
@@ -18,16 +19,23 @@ class NoteCollectionFlowLayout: UICollectionViewFlowLayout {
         itemSize = CGSize(width: contentWidth, height: contentHeight)
         /*
          width = 390
+         
+        card peek width = ukuran dari card sebelah, semakin besar berarti peek ke card sebelahnya bakal lebih besar juga
+         nanti digunakan untuk minimum line spacing/jarak antara card
+         
          peeking width = 390/5 = 78point
          peeking width = 390/6 = 65point
          peeking width = 390/9 = 43point
          */
         let cardPeekWidth = itemSize.width / 9
-        //horizontal margin, insets = margin
+        //horizontal margin, insets = margin, this case 0
         let horizontalInset = (collectionView.frame.size.width - itemSize.width) / 2
         //set margin
         collectionView.contentInset = UIEdgeInsets(top: 0, left: horizontalInset, bottom: 0, right: horizontalInset)
-        minimumLineSpacing = horizontalInset - cardPeekWidth
         
+        //jarak antara 1 card ke card lain semakin kecil, semakin dekat jaraknya
+        minimumLineSpacing = horizontalInset - cardPeekWidth
+        print("HOrinset: \(minimumLineSpacing)")
+
     }
 }
--- improv/NoteViewController.swift
+++ improv/NoteViewController.swift
@@ -94,6 +94,7 @@ class NoteViewController: UIViewController, AVAudioRecorderDelegate {
         guard let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else{
             return
         }
+        checkMicrophoneAccess()
         let audioSession = AVAudioSession.sharedInstance()
         do{
             try audioSession.setCategory(AVAudioSession.Category(rawValue: AVAudioSession.Category.playAndRecord.rawValue), mode: .default)
@@ -101,71 +102,106 @@ class NoteViewController: UIViewController, AVAudioRecorderDelegate {
         catch{
             print("Audio: ERROR")
         }
+        if AVAudioSession.sharedInstance().recordPermission == AVAudioSession.RecordPermission.granted {
+            if isRehearsal == false{
+                rehearsalDurationLabel.text = rehearseDuration
+                let date = Date()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyymmddHHmmss"
+                let dateStr = dateFormatter.string(from: date)
+                audioFileName = "\(foldersArr[folderIndex].name!) Rehearsal \(dateStr)"
+                audioFileNamewithExtension = "\(audioFileName).m4a"
+                
 
-        if isRehearsal == false{
-            rehearsalDurationLabel.text = rehearseDuration
-            let date = Date()
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyymmddHHmmss"
-            let dateStr = dateFormatter.string(from: date)
-            audioFileName = "\(foldersArr[folderIndex].name!) Rehearsal \(dateStr)"
-            audioFileNamewithExtension = "\(audioFileName).m4a"
-            
-
-            rehearseButton.setTitle("Stop", for: .normal)
-            isRehearsal = true
-            
-            audioFileURL = directoryURL.appendingPathComponent(audioFileNamewithExtension)
-
-            let recorderSetting = [AVFormatIDKey: NSNumber(value: kAudioFormatMPEG4AAC as UInt32),
-                                 AVSampleRateKey: 44100.0,
-                           AVNumberOfChannelsKey: 2,
-                             AVEncoderBitRateKey: 320000,
-                        AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue] as [String : Any]
-            audioRecorder = try? AVAudioRecorder(url: audioFileURL, settings: recorderSetting)
-            audioRecorder?.delegate = self
-            audioRecorder?.isMeteringEnabled = true
-            if !audioRecorder!.isRecording {
-                do{
-                    try audioSession.setActive(true)
-                } catch _ {
-                    print("Audio: ERROR")
-                }
-                audioRecorder!.record()
-                rehearsalDurationLabel.isHidden = false
-                 timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true){ [self]
-                    timer in
-                    
-                    self.duration += 1
-                    self.rehearseDuration = self.convertSecondToMinutes(seconds: duration)
-                    self.rehearsalDurationLabel.text = rehearseDuration
-                }
+                rehearseButton.setTitle("Stop", for: .normal)
                 isRehearsal = true
-            }
-        }
-        else{
-            rehearseButton.setTitle("Rehearse", for: .normal)
-            isRehearsal = false
-            
-            if audioRecorder!.isRecording {
-                audioRecorder!.stop()
-                timer?.invalidate()
-                do{
-                    try audioSession.setActive(false)
+                
+                audioFileURL = directoryURL.appendingPathComponent(audioFileNamewithExtension)
+
+                let recorderSetting = [AVFormatIDKey: NSNumber(value: kAudioFormatMPEG4AAC as UInt32),
+                                     AVSampleRateKey: 44100.0,
+                               AVNumberOfChannelsKey: 2,
+                                 AVEncoderBitRateKey: 320000,
+                            AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue] as [String : Any]
+                audioRecorder = try? AVAudioRecorder(url: audioFileURL, settings: recorderSetting)
+                audioRecorder?.delegate = self
+                audioRecorder?.isMeteringEnabled = true
+                if !audioRecorder!.isRecording {
+                    do{
+                        try audioSession.setActive(true)
+                    } catch _ {
+                        print("Audio: ERROR")
+                    }
+                    audioRecorder!.record()
+                    rehearsalDurationLabel.isHidden = false
+                     timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true){ [self]
+                        timer in
+                        
+                        self.duration += 1
+                        self.rehearseDuration = self.convertSecondToMinutes(seconds: duration)
+                        self.rehearsalDurationLabel.text = rehearseDuration
+                    }
+                    isRehearsal = true
                 }
-                catch _ {
-                    print("Audio: ERROR")
+            }
+            else{
+                rehearseButton.setTitle("Rehearse", for: .normal)
+                isRehearsal = false
+                
+                if audioRecorder!.isRecording {
+                    audioRecorder!.stop()
+                    timer?.invalidate()
+                    do{
+                        try audioSession.setActive(false)
+                    }
+                    catch _ {
+                        print("Audio: ERROR")
+                    }
+                }
+    //            if somehow mau pakai userdefault, untuk filepathnya append audio filename ->  /audioFileName
+    //            let fileLocation = "\(directoryURL.path)/\(audioFileNamewithExtension)"
+                rehearsalsArr.append(Rehearsal(name: audioFileName, duration: rehearseDuration, filePath: audioFileNamewithExtension))
+                if let encodedRehearsals = try? encoder.encode(rehearsalsArr){
+                    defaults.set(encodedRehearsals, forKey: "Rehearsal")
                 }
             }
-//            if somehow mau pakai userdefault, untuk filepathnya append audio filename ->  /audioFileName
-//            let fileLocation = "\(directoryURL.path)/\(audioFileNamewithExtension)"
-            rehearsalsArr.append(Rehearsal(name: audioFileName, duration: rehearseDuration, filePath: audioFileNamewithExtension))
-            if let encodedRehearsals = try? encoder.encode(rehearsalsArr){
-                defaults.set(encodedRehearsals, forKey: "Rehearsal")
-            }
+
         }
     }
-    
+    func checkMicrophoneAccess() {
+        // Check Microphone Authorization
+        let recordPermission = AVAudioSession.sharedInstance().recordPermission
+        if recordPermission == AVAudioSession.RecordPermission.granted{
+            print("Permission: Microphone granted")
+        }
+        else if recordPermission == AVAudioSession.RecordPermission.denied {
+            let alert = UIAlertController(title: "Microphone Permission Required", message: "Improv is Not Authorized to Access the Microphone!", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            //open settings
+            let settingAction = UIAlertAction(title: "Settings", style: .default) { (_) in
+                guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else{
+                    return
+                }
+                if UIApplication.shared.canOpenURL(settingsURL){
+                    UIApplication.shared.open(settingsURL, completionHandler: nil)
+                }
+            }
+            alert.addAction(cancelAction)
+            alert.addAction(settingAction)
+            present(alert,animated: true)
+            return
+        }
+        else if recordPermission == AVAudioSession.RecordPermission.undetermined{
+            AVAudioSession.sharedInstance().requestRecordPermission({ (granted) in
+                if granted {
+                    print("Permission granted")
+                } else {
+                    print("Pemission not granted")
+                }
+            })
+        }
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let dest = segue.destination as! ManageNoteViewController
         dest.folderIndex = folderIndex
@@ -187,15 +223,22 @@ class NoteViewController: UIViewController, AVAudioRecorderDelegate {
 //MARK: Scrolling behaviour
 extension NoteViewController{
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        /*
+         Ketika selesai drag, current snap and center current note
+         */
 
         guard scrollView == noteCollectionView else{
             return
         }
         targetContentOffset.pointee = scrollView.contentOffset
         let flowLayout = noteCollectionView.collectionViewLayout as! NoteCollectionFlowLayout
-        let cellWidthAndSpacing = flowLayout.itemSize.width + flowLayout.minimumLineSpacing
 
+        //untuk set current cell width dan spacing
+        let cellWidthAndSpacing = flowLayout.itemSize.width + flowLayout.minimumLineSpacing
+        
+        //posisi dari content relatif terhadap origin coordinates
         let offset = targetContentOffset.pointee
+        //scroll speed
         let horizontalSwipeVelocity = velocity.x
 
         var selectedIndex = currentSelectedIndex
@@ -224,6 +267,8 @@ extension NoteViewController{
         else if selectedIndex < 0 {
             selectedIndex = 0
         }
+        
+        //untuk nge fokus content ketika lagi di drag
         let selectedIndexPath = IndexPath(row: selectedIndex, section: 0)
         flowLayout.collectionView?.scrollToItem(at: selectedIndexPath, at: .centeredHorizontally, animated: true)
 
@@ -231,7 +276,7 @@ extension NoteViewController{
         let previousSelectedIndex = IndexPath(row: Int(currentSelectedIndex), section: 0)
         let previousSelectedCard = noteCollectionView.cellForItem(at: previousSelectedIndex) as! NoteCollectionView
         let nextSelectedCard = noteCollectionView.cellForItem(at: selectedIndexPath) as! NoteCollectionView
-//
+
         currentSelectedIndex = selectedIndexPath.row
 
         previousSelectedCard.shrinkCard()
@@ -240,6 +285,9 @@ extension NoteViewController{
         
     }
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        /*
+         Ketika nge drag, current note dishrink
+         */
         let currentCell = noteCollectionView.cellForItem(at: IndexPath(row: currentSelectedIndex, section: 0)) as! NoteCollectionView
         currentCell.shrinkCard()
     }
